88f1ede00e5a748a6aefa917a2653d2cf8cc1ca0
purvesh-jaiswal <purvesh@appliedcloudcomputing.com>
2017-01-16 23:30:06 +0530

    first commit for maven project

---
+++ mynewmaven/src/test/java/com/orag/example/HelloTest.java
@@ -0,0 +1,18 @@
+package com.orag.example;
+
+
+import org.testng.annotations.Test;
+
+public class HelloTest {
+	
+	@Test
+	public void  start(){
+		
+		System.out.println("hello maven");
+		
+		
+		
+	
+	}
+
+}
